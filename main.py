9cfe92a47bc85b4c29107b959b380d44f92f1455
Mark Lenhardt <SolutionsExcite@gmail.com>
2024-08-17 15:00:29 -0500

    added tee to utilize hardware accelerated h265 encoding and saving to file while running.

--- main.py
+++ main.py
@@ -13,31 +13,45 @@ src = Gst.ElementFactory.make("v4l2src", "source")
 capsfilter1 = Gst.ElementFactory.make("capsfilter", "capsfilter1")
 videoscale = Gst.ElementFactory.make("videoscale", "videoscale")
 capsfilter2 = Gst.ElementFactory.make("capsfilter", "capsfilter2")
+tee = Gst.ElementFactory.make("tee", "tee")
+queue1 = Gst.ElementFactory.make("queue", "queue1")
+queue2 = Gst.ElementFactory.make("queue", "queue2")
 sink = Gst.ElementFactory.make("glimagesink", "sink")
+encoder = Gst.ElementFactory.make("mpph265enc", "encoder")
+parser = Gst.ElementFactory.make("h265parse", "parser")
+muxer = Gst.ElementFactory.make("matroskamux", "muxer")
+filesink = Gst.ElementFactory.make("filesink", "filesink")
 
-# Set the device property on the source element
+# Set properties
 src.set_property("device", "/dev/video0")
-
-# Set the caps for the first capsfilter
 caps1 = Gst.Caps.from_string("video/x-raw,width=1920,height=1080")
 capsfilter1.set_property("caps", caps1)
-
-# Set the caps for the second capsfilter
 caps2 = Gst.Caps.from_string("video/x-raw,width=1920,height=1080")
 capsfilter2.set_property("caps", caps2)
+filesink.set_property("location", "output.mkv")
 
 # Add all elements to the pipeline
-pipeline.add(src)
-pipeline.add(capsfilter1)
-pipeline.add(videoscale)
-pipeline.add(capsfilter2)
-pipeline.add(sink)
+elements = [src, capsfilter1, videoscale, capsfilter2, tee, queue1, queue2, 
+            sink, encoder, parser, muxer, filesink]
+for element in elements:
+    pipeline.add(element)
 
 # Link the elements
 src.link(capsfilter1)
 capsfilter1.link(videoscale)
 videoscale.link(capsfilter2)
-capsfilter2.link(sink)
+capsfilter2.link(tee)
+
+# Link one branch of the tee to the display sink
+tee.link(queue1)
+queue1.link(sink)
+
+# Link the other branch of the tee to the encoder, parser, muxer, and file output
+tee.link(queue2)
+queue2.link(encoder)
+encoder.link(parser)
+parser.link(muxer)
+muxer.link(filesink)
 
 # Start playing
 pipeline.set_state(Gst.State.PLAYING)
